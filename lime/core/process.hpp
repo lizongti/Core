81607816854bbfbea97330a93fabccf4249afed3
lizongti <44680454+lizongti@users.noreply.github.com>
2020-03-21 01:55:09 +0800

    fix linking bug

--- lime/core/process.hpp
+++ lime/core/process.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <thread>
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 #include "singleton.hpp"
 #include "service.hpp"
@@ -30,19 +31,20 @@ class process
 public:
 	process &options(int argc, char *argv[])
 	{
+		std::cout << boost::filesystem::initial_path<boost::filesystem::path>().string() << std::endl;
 		boost::program_options::variables_map variables_map;
-		// boost::program_options::options_description option_description("Options");
-		// option_description.add_options()("help,h", "describe arguments")("thread,t", boost::program_options::value<uint32_t>(), "thread")("service,sl", boost::program_options::value<uint32_t>(), "service")("bootstrap,s", boost::program_options::value<std::string>(), "bootstrap");
+		boost::program_options::options_description option_description("Allowed options");
+		option_description.add_options()("help,h", "describe arguments")("thread,t", boost::program_options::value<uint32_t>(), "thread")("service,sl", boost::program_options::value<uint32_t>(), "service")("bootstrap,s", boost::program_options::value<std::string>(), "bootstrap");
 		boost::program_options::positional_options_description positional_options_description;
 		positional_options_description.add("bootstrap", -1);
-
 		try
 		{
-			// boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(option_description).positional(positional_options_description).run(), variables_map);
+			boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(option_description).positional(positional_options_description).run(), variables_map);
 			boost::program_options::notify(variables_map);
 		}
-		catch (std::exception const &)
+		catch (std::exception const &e)
 		{
+			std::cerr << e.what() << std::endl;
 			std::cerr << "Incorrect command line syntax." << std::endl;
 			std::cerr << "Use '--help' for a list of options." << std::endl;
 			exit(1);
