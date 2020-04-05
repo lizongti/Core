10f4ce2a71666b05265daf2d305f9da9696cb979
lizongti <44680454+lizongti@users.noreply.github.com>
2020-04-05 20:58:47 +0800

    Use common functions in dll.

---
+++ ants/def/functions.h
@@ -0,0 +1,14 @@
+static int(__cdecl *start)(const char *module_name,
+                           const char *service_name);
+static int(__cdecl *send)(void *context,
+                          const char *source,
+                          const char *destination,
+                          void *data);
+static int(__cdecl *stop)(const char *service_name);
+
+void init_functions(void *function_array[])
+{
+    start = function_array[0];
+    send = function_array[1];
+    stop = function_array[2];
+}
--- src/bootstrap/boostrap.c
+++ src/bootstrap/boostrap.c
@@ -2,13 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static int(__cdecl *start)(const char *module_name,
-                           const char *service_name);
-static int(__cdecl *send)(void *context,
-                          const char *source,
-                          const char *destination,
-                          void *data);
-static int(__cdecl *stop)(const char *service_name);
+#include <ants/def/functions.h>
 
 struct bootstrap_context
 {
@@ -17,10 +11,7 @@ struct bootstrap_context
 
 void *__cdecl create(const char *service_name, void *function_array[])
 {
-    start = function_array[0];
-    send = function_array[1];
-    stop = function_array[2];
-
+    init_functions(function_array);
     struct bootstrap_context *context = malloc(sizeof(struct bootstrap_context));
     context->id = 0;
     return context;
